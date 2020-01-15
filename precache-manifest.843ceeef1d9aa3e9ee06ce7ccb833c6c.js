4ca11c3d6f3767363af5fcd42b2211efbaa31a28
Modhi Aldughaiyem <modhi1993@gmail.com>
2020-01-15 16:46:09 +0300

    Updates

---
+++ precache-manifest.843ceeef1d9aa3e9ee06ce7ccb833c6c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03c5c2fc4d0638a159360f86de3cf732",
+    "url": "/project-3-second/index.html"
+  },
+  {
+    "revision": "a8f38bb8508e1152e1a8",
+    "url": "/project-3-second/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "d35530478fba26153bd0",
+    "url": "/project-3-second/static/css/main.11b8bc6b.chunk.css"
+  },
+  {
+    "revision": "a8f38bb8508e1152e1a8",
+    "url": "/project-3-second/static/js/2.ded0def2.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/project-3-second/static/js/2.ded0def2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d35530478fba26153bd0",
+    "url": "/project-3-second/static/js/main.3eea27d3.chunk.js"
+  },
+  {
+    "revision": "f08746516a9b1b19ee93",
+    "url": "/project-3-second/static/js/runtime-main.031393a6.js"
+  },
+  {
+    "revision": "6a5f1ef62928dea5b9f598d43463f194",
+    "url": "/project-3-second/static/media/home.6a5f1ef6.JPG"
+  }
+]);
